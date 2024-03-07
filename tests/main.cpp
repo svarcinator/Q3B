3fd6675561cfe6c526fbec4cded4ea9d240110fa
Tereza <schwarzova.tereza9@gmail.com>
2024-03-07 12:11:44 +0100

    Added interval tests

--- tests/main.cpp
+++ tests/main.cpp
@@ -7,8 +7,12 @@
 #include "antlr4-runtime.h"
 #include "SMTLIBv2Lexer.h"
 #include "SMTLIBv2Parser.h"
+#include "../lib/BWChangeEffect.h"
+#include <vector>
 
 using namespace antlr4;
+
+typedef std::pair<int, int> Interval;
 
 Model model;
 
@@ -359,6 +363,8 @@ Result SolveWithAllApproxLazy(std::string filename,  int precision = 0)
     return interpreter.Run(tree->script());
 }
 
+
+
 TEST_CASE( "Problematic - Without approximations", "[noapprox]" )
 {
     REQUIRE( SolveWithoutApprox("../tests/data/AR-fixpoint-1.smt2") == UNSAT );
@@ -380,6 +386,15 @@ TEST_CASE( "Problematic - Without approximations", "[noapprox]" )
     REQUIRE( SolveWithBothLimitApproxLazy("../tests/problematic/check_bvsge_bvand_64bit.smt2", OVERAPPROXIMATION) == UNSAT );
     REQUIRE( SolveWithoutApproxLazy("../tests/problematic/check_bvsge_bvand_64bit.smt2") == UNSAT );
     REQUIRE( SolveWithAllApproxLazy("../tests/problematic/check_bvslt_bvor_64bit-djusted.smt2") == UNSAT );
+    
+   
+}
+
+TEST_CASE( "Wintersteiger - problematic", "[bothlimitapprox]" )
+{
+    //REQUIRE( SolveWithBothLimitApprox("../tests/wintersteiger/fixpoint-small-pipeline-fixpoint-5.smt2", OVERAPPROXIMATION) == SAT );
+    
+    //REQUIRE( SolveWithBothLimitApprox("../tests/wintersteiger/fixpoint-small-pipeline-fixpoint-5.smt2", UNDERAPPROXIMATION) == SAT );
     
    
 }
@@ -571,4 +586,150 @@ TEST_CASE( "Models", "[models]" )
     REQUIRE( std::get<1>(model["x"]) == std::vector<bool>{false, false, true, true} );
     REQUIRE( std::get<1>(model["y"]) == std::vector<bool>{false, false, true, true} );
     REQUIRE( std::get<1>(model["z"]) == std::vector<bool>{true, false, false, true} );
+}
+
+
+
+std::vector<Interval> getRightShift(const std::vector<Interval>& vec, unsigned int offset) 
+{
+    return BWChangeEffect::ShiftRight(vec , offset);
+}
+
+std::vector<Interval> getLefttShift(const std::vector<Interval>& vec, unsigned int offset) 
+{
+    return BWChangeEffect::ShiftLeft(vec , offset);
+}
+
+TEST_CASE("Right Shift Test")
+{
+	
+	// CASE 1
+	std::vector<Interval> vec ={{4,2}};
+	std::vector<Interval> res ={{2,0}};
+	std::vector<Interval> res_overflow ={{1,0}};
+	std::vector<Interval> res_full_overflow = {};
+		
+	REQUIRE (getRightShift(vec, 2) == res);
+	REQUIRE (getRightShift(vec, 0) == vec);
+	REQUIRE (getRightShift(vec, 3) == res_overflow );
+	REQUIRE (getRightShift(vec, 12) == res_full_overflow );
+	
+	// CASE 2
+	std::vector<Interval> vec2 ={{INT_MAX,2}};
+	std::vector<Interval> res2 ={{INT_MAX,0}};
+	
+	REQUIRE (getRightShift(vec2, 2) == res2);
+	REQUIRE (getRightShift(vec2, 5) == res2);
+	
+	// CASE 3
+	std::vector<Interval> vec3 ={{INT_MAX,0}};
+	std::vector<Interval> res3 ={{INT_MAX,0}};
+	
+	REQUIRE (getRightShift(vec3, 2) == res3);
+	REQUIRE (getRightShift(vec3, 5) == res3);
+}
+
+TEST_CASE("Left Shift Test")
+{
+	
+	// CASE 1
+	std::vector<Interval> vec ={{4,2}};
+	std::vector<Interval> res ={{6,4}};
+		
+	REQUIRE (getLefttShift(vec, 2) == res);
+	REQUIRE (getLefttShift(vec, 0) == vec);
+	
+	// CASE 2
+	std::vector<Interval> vec2 ={{INT_MAX,2}};
+	std::vector<Interval> res2 ={{INT_MAX,4}};
+	
+	REQUIRE (getLefttShift(vec2, 2) == res2);
+	
+	// CASE 3
+	std::vector<Interval> vec3 ={{INT_MAX,0}};
+	std::vector<Interval> res3 ={{INT_MAX,2}};
+	
+	REQUIRE (getLefttShift(vec3, 2) == res3);
+}
+
+TEST_CASE(" Shift Test")
+{
+	
+	// CASE 1
+	std::vector<Interval> vec ={{4,2}};
+	std::vector<Interval> res =BWChangeEffect::EffectOfShift(vec, 2);
+	std::vector<Interval> res_right = {{3,1}};
+		
+	REQUIRE (getLefttShift(vec, 2) == res);
+	REQUIRE (BWChangeEffect::EffectOfShift(vec, 0) == vec);
+	REQUIRE (BWChangeEffect::EffectOfShift(vec, -1) == res_right);
+	
+	// CASE 2
+	std::vector<Interval> vec2 ={{INT_MAX,2}};
+	std::vector<Interval> res2 ={{INT_MAX,4}};
+	std::vector<Interval> res2_overflow ={{INT_MAX,0}};
+	
+	
+	REQUIRE (BWChangeEffect::EffectOfShift(vec2, 2) == res2);
+	REQUIRE (BWChangeEffect::EffectOfShift(vec2, -3) == res2_overflow);
+	
+	// CASE 3
+	std::vector<Interval> vec3 ={{INT_MAX,0}};
+	std::vector<Interval> res3 ={{INT_MAX,2}};
+	
+	REQUIRE (BWChangeEffect::EffectOfShift(vec3, 2) == res3);
+	REQUIRE (BWChangeEffect::EffectOfShift(vec3, -2) == vec3);
+	
+}
+
+
+TEST_CASE(" Extract Test")
+{	
+	// CASE 1 extract [5, 0]
+	std::vector<Interval> vec ={{7,2}};
+	std::vector<Interval> res ={{5,2}};
+	REQUIRE(BWChangeEffect::EffectOnExtract(vec, 0, 6) == res);
+	
+	// CASE 2 extract [5, 3]
+	std::vector<Interval> vec2 ={{7,2}};
+	std::vector<Interval> res2 ={{2,0}};
+	REQUIRE(BWChangeEffect::EffectOnExtract(vec2, 3, 3) == res2);
+	
+	// CASE 3 extract [7,5]
+	
+	std::vector<Interval> vec3 ={{7,2}};
+	std::vector<Interval> res3 ={{2, 0}};
+	REQUIRE(BWChangeEffect::EffectOnExtract(vec3, 5, 3) == res3);
+	
+	
+	// CASE 4 extract [7, 0]
+	
+	std::vector<Interval> vec4 ={{7,2}};
+	std::vector<Interval> res4 ={{7,2}};
+	REQUIRE(BWChangeEffect::EffectOnExtract(vec4, 0, 8) == res4);
+	
+	// CASE 5 extraxt [4,1]
+	std::vector<Interval> vec5 ={{INT_MAX,0}};
+	std::vector<Interval> res5 ={{3,0}};
+	
+	REQUIRE(BWChangeEffect::EffectOnExtract(vec5, 1, 4) == res5);
+	
+	// CASE 6 extraxt [4,1]
+	std::vector<Interval> vec6 ={{INT_MAX,0}};
+	std::vector<Interval> res6={{3,0}};
+	
+	REQUIRE(BWChangeEffect::EffectOnExtract(vec6, 1, 4) == res6);
+	
+	// CASE 7 extract [5, 0]
+	std::vector<Interval> vec7 ={{7,6}, {1, 0}};
+	std::vector<Interval> res7={{1,0}};
+	
+	REQUIRE(BWChangeEffect::EffectOnExtract(vec7, 0, 6) == res7);
+	
+	// CASE 8 extract [5, 3]
+	std::vector<Interval> vec8 ={{7,6}, {1, 0}};
+	std::vector<Interval> res8={};
+	
+	REQUIRE(BWChangeEffect::EffectOnExtract(vec8, 3, 3) == res8);
+	
 }
